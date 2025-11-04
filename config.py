622928c1a1aaf1c59d49f3093a20423a3ec9a445
phant <phant890516@gmail.com>
2025-11-05 02:09:13 +0900

    session

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ from datetime import timedelta
 class Config:
     # Flaskのセッション機能や特定の拡張機能で暗号化キーとして使用される
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'seucrber_tkikeays_ih28'
-    PERMANENT_SESSION_LIFETIME = timedelta(minutes=60)
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=240)
