ad2eef6ef656ce1e3fdc93c9d28c68606d5a6403
phant <phant890516@gmail.com>
2025-11-13 11:02:59 +0900

    とりひき

--- app/views/mypage.py
+++ app/views/mypage.py
@@ -133,7 +133,11 @@ def get_transaction_info(id):
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     if evaluation is not None:
+=======
+    if evaluation:
+>>>>>>> Stashed changes
 =======
     if evaluation:
 >>>>>>> Stashed changes
@@ -166,7 +170,10 @@ def get_transaction_info(id):
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
    
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
 =======
